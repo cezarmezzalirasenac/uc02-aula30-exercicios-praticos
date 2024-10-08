52ed13d1ade3a3f8400861560a7686afc19b217d
Cezar Augusto Mezzalira <cezar.mezzalira@docente.pr.senac.br>
2024-10-07 21:55:42 -0300

    Resolução exercícios

--- exercicio-01/script.js
+++ exercicio-01/script.js
@@ -1,19 +1,43 @@
-const selectOperacao = document.getElementById('select-operacao')
-const btnCalcular = document.getElementById('btn-calcular')
+const selectOperacao = document.getElementById("select-operacao")
+const btnCalcular = document.getElementById("btn-calcular")
 const divRoot = document.getElementById("root")
 const textResultado = document.createElement("h3")
 divRoot.appendChild(textResultado)
 
-btnCalcular.addEventListener('click', (event) => {
+const primeiroNumero = document.getElementById("primeiro-numero")
+const segundoNumero = document.getElementById("segundo-numero")
+
+btnCalcular.addEventListener("click", (event) => {
   event.preventDefault()
   // ENTRADA
   // Pegar os dois valores e a operação
+  const primeiroValor = Number(primeiroNumero.value)
+  const segundoValor = Number(segundoNumero.value)
 
+  if (primeiroValor === 0) {
+    alert("Informe o primeiro valor")
+    return
+  }
+
+  if (segundoValor === 0) {
+    alert("Informe o segundo valor")
+    return
+  }
+  // PROCESSAMENTO
   const operacao = selectOperacao.value
-  console.log(operacao)
 
   // lógica de fazer a operação baseado na operação selecionada
   let resultado = 0
+
+  if (operacao === "somar") {
+    resultado = primeiroValor + segundoValor
+  } else if (operacao === "subtrair") {
+    resultado = primeiroValor - segundoValor
+  } else if (operacao === "multiplicar") {
+    resultado = primeiroValor * segundoValor
+  } else if ((operacao === "dividir") && (segundoValor > 0)) {
+    resultado = primeiroValor / segundoValor
+  }
 
   // Mostrar o resultado 
   textResultado.textContent = resultado
